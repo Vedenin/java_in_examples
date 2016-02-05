da8db031c0f8286255843eb4839b2e1f45e36d95
vedenin <vedenin1980@inbox.ru>
2016-02-05 23:26:55 +0100

    [JPO-18] Fix page coding problem in russian version

--- other/src/main/java/com/github/vedenin/rus/arrays/ConvertArrayToStringAndPrintTest.java
+++ other/src/main/java/com/github/vedenin/rus/arrays/ConvertArrayToStringAndPrintTest.java
@@ -5,39 +5,39 @@ import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 
 /**
- *  ??????? ?????????????? ??????? ? ?????? ? ????? ???? ????????? ??????? ? ???
+ *  Тестирование превращения массива в строку для печати
  *
  * Created by vedenin on 04.02.16.
  */
-public class ConvertArrayToStringTest {
+public class ConvertArrayToStringAndPrintTest {
     public static void main(String[] s) throws Exception {
 
-        /* (JDK 5) ????????? Arrays.toString ??? Arrays.deepToString */
+        /* (JDK 5) Использование Arrays.toString и Arrays.deepToString */
         // simple array
         String[] array = new String[]{"John", "Mary", "Bob"};
-        System.out.println(Arrays.toString(array)); // ??????????: [John, Mary, Bob]
+        System.out.println(Arrays.toString(array)); // Напечатает: [John, Mary, Bob]
 
         // nested array
         String[][] deepArray = new String[][]{{"John", "Mary"}, {"Alice", "Bob"}};
-        System.out.println(Arrays.toString(deepArray)); // ??????????: [[Ljava.lang.String;@106d69c, [Ljava.lang.String;@52e922]
-        System.out.println(Arrays.deepToString(deepArray)); // ??????????: [[John, Mary], [Alice, Bob]]
+        System.out.println(Arrays.toString(deepArray)); // Напечатает: [[Ljava.lang.String;@106d69c, [Ljava.lang.String;@52e922]
+        System.out.println(Arrays.deepToString(deepArray)); // Напечатает: [[John, Mary], [Alice, Bob]]
 
         //double Array
         double[] arrayGiven = {7.0, 9.0, 5.0, 1.0, 3.0};
-        System.out.println(Arrays.toString(arrayGiven)); //  ??????????: [7.0, 9.0, 5.0, 1.0, 3.0 ]
+        System.out.println(Arrays.toString(arrayGiven)); //  Напечатает: [7.0, 9.0, 5.0, 1.0, 3.0 ]
 
         //int Array
         int[] arrayInt = {7, 9, 5, 1, 3};
-        System.out.println(Arrays.toString(arrayInt)); //  ??????????: [7, 9, 5, 1, 3 ]
+        System.out.println(Arrays.toString(arrayInt)); //  Напечатает: [7, 9, 5, 1, 3 ]
 
-        /* (JDK 8) ????????? Stream API */
-        Arrays.asList(array).stream().forEach(System.out::print); //  ??????????: JohnMaryBob
+        /* (JDK 8) Использование Stream API */
+        Arrays.asList(array).stream().forEach(System.out::print); //  Напечатает: JohnMaryBob
         System.out.println();
         Arrays.asList(deepArray).stream().forEach(s1 -> Arrays.asList(s1).stream().forEach(System.out::print));
         System.out.println();
-        DoubleStream.of(arrayGiven).forEach((d) -> System.out.print(d + " "));  //  ??????????: 7.0 9.0 5.0 1.0 3.0
+        DoubleStream.of(arrayGiven).forEach((d) -> System.out.print(d + " "));  //  Напечатает: 7.0 9.0 5.0 1.0 3.0
         System.out.println();
-        IntStream.of(arrayInt).forEach(System.out::print);  //  ??????????: 79513
+        IntStream.of(arrayInt).forEach(System.out::print);  //  Напечатает: 79513
 
 
     }
--- other/src/main/java/com/github/vedenin/rus/string_and_stream/StringUtilsTest.java
+++ other/src/main/java/com/github/vedenin/rus/string_and_stream/StringUtilsTest.java
@@ -4,7 +4,7 @@ import com.google.common.base.Strings;
 import org.apache.commons.lang.StringUtils;
 
 /**
- * ???????????? ?????? ???????? ????????? ??? ?????? ?????
+ * Тестирование разных способов получения isEmpty
  *
  * Created by vvedenin on 2/5/2016.
  */
@@ -23,12 +23,18 @@ public class StringUtilsTest {
         if(Strings.isNullOrEmpty(emptyString)) {
             System.out.println("test isNullOrEmpty of Guava - passed");
         }
-        if(org.springframework.util.StringUtils.hasText(emptyString)) {
-            System.out.println("test hasText of Spring - passed");
-        }
+    }
+
+    private static void isBlank() {
+        String emptyString = "   ";
+        boolean isApache = StringUtils.isBlank(emptyString);
+        boolean hasText = org.springframework.util.StringUtils.hasText(emptyString);
+        System.out.println("isBlank of Apache = " + isApache);
+        System.out.println("hasText of Spring = " + hasText);
     }
 
     public static void main(String[] args) {
         isEmptyTest();
+        isBlank();
     }
 }
