e66c14b3c35a4aee7933febf2b0355584fec20fe
vedenin <vedenin1980@inbox.ru>
2016-04-08 00:28:38 +0200

    JPO-47 Add machine-learning module

--- machine-learning/src/com/github/vedenin/machine_learning/Test.java
+++ machine-learning/src/com/github/vedenin/machine_learning/Test.java
@@ -1,7 +1,26 @@
 package com.github.vedenin.machine_learning;
 
+import smile.data.AttributeDataset;
+import smile.data.parser.ArffParser;
+
+import java.io.IOException;
+import java.text.ParseException;
+
 /**
  * Created by vedenin on 07.04.16.
  */
 public class Test {
+
+    private void test() throws IOException, ParseException {
+        ArffParser arffParser = new ArffParser();
+        arffParser.setResponseIndex(4);
+        AttributeDataset weather = arffParser.parse(this.getClass().getResourceAsStream("/smile/data/weka/weather.nominal.arff"));
+        double[][] x = weather.toArray(new double[weather.size()][]);
+        int[] y = weather.toArray(new int[weather.size()]);
+    }
+
+    public static void main(String[] s) throws Exception {
+        Test cls = new Test();
+        cls.test();
+    }
 }
