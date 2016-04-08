a4f556f594c9d9b329d38dbf25fb0835766043bf
vvedenin <vvedenin@luxoft.com>
2016-04-08 12:32:00 +0200

    JPO-48 Fix GithubDownLoadTest and SentenceDetectors

--- html_parser/src/com.github/vedenin/url_parser/GithubDownLoadTests.java
+++ html_parser/src/com.github/vedenin/url_parser/GithubDownLoadTests.java
@@ -3,13 +3,15 @@ package com.github.vedenin.url_parser;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.parser.Tag;
 import org.jsoup.select.Elements;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
-import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by vedenin on 07.04.16.
@@ -48,9 +50,10 @@ public class GithubDownLoadTests {
     private static void testHtmlParser(String url) throws Exception {
         Document doc = Jsoup.connect(url).userAgent(USER_AGENT).cookie("auth", "token")
                 .timeout(30000).get();
-        Elements div = doc.select("#readme").select("ol").select("li");
+        Elements div = doc.select("#readme");
 
-        printElements(div);
+        //printElements(div);
+        work(div);
     }
 
     private static void printElements(Elements children) {
@@ -66,5 +69,58 @@ public class GithubDownLoadTests {
     public static void main(String[] s) throws Exception {
         initHTTPSDownload();
         testHtmlParser("https://github.com/Vedenin/useful-java-links/blob/master/readme.md");
+    }
+
+
+    private static final Tag H1 = Tag.valueOf("h1");
+    private static final Tag H2 = Tag.valueOf("h2");
+    private static final Tag H3 = Tag.valueOf("h3");
+    private static final Tag H4 = Tag.valueOf("h4");
+    private static final Tag H5 = Tag.valueOf("h5");
+    private static final Tag H6 = Tag.valueOf("h6");
+
+
+    private static boolean isHeader(Tag tag) {
+        return H1.equals(tag) || H2.equals(tag) || H3.equals(tag) || H4.equals(tag) || H5.equals(tag) || H6.equals(tag);
+    }
+    private static List<LinkContainer> work(Elements elements) {
+        List<LinkContainer> result = new ArrayList<>(elements.size());
+        String currentCategory = null;
+        for(Element element: elements) {
+            Tag tag = element.tag();
+            if(isHeader(tag)) {
+                currentCategory = element.text();
+                System.out.println(currentCategory);
+            }
+            work(element.children());
+        }
+        return result;
+    }
+
+    private static class LinkContainer {
+        private String category;
+        private String name;
+        private String url;
+        private String description;
+        private String star;
+        private String stackOverflow;
+        private String license;
+        private String licenseUrl;
+        private String site;
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "category='" + category + '\'' +
+                    ", name='" + name + '\'' +
+                    ", url='" + url + '\'' +
+                    ", description='" + description + '\'' +
+                    ", star='" + star + '\'' +
+                    ", stackOverflow='" + stackOverflow + '\'' +
+                    ", license='" + license + '\'' +
+                    ", licenseUrl='" + licenseUrl + '\'' +
+                    ", site='" + site + '\'' +
+                    '}';
+        }
     }
 }
--- natural-language-processing/src/main/java/com/github/vedenin/nlp/SentenceDetectors.java
+++ natural-language-processing/src/main/java/com/github/vedenin/nlp/SentenceDetectors.java
@@ -3,7 +3,6 @@ package com.github.vedenin.nlp;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
 import edu.stanford.nlp.util.CoreMap;
 import opennlp.tools.sentdetect.SentenceDetectorME;
 import opennlp.tools.sentdetect.SentenceModel;
@@ -11,7 +10,6 @@ import opennlp.tools.util.Span;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
@@ -55,18 +53,34 @@ public class SentenceDetectors {
     }
 
     public static void main(String[] strings) throws Exception {
-        String text = "“But I don’t want to go among mad people,” Alice remarked. " +
-                "“Oh, you can’t help that,” said the Cat: “we’re all mad here. I’m mad. You’re mad.” " +
-                "“How do you know I’m mad?” said Alice. " +
-                "“You must be,” said the Cat, “or you wouldn’t have come here.”";
+        String text = "'But I don’t want to go among mad people,' Alice remarked. " +
+                "'Oh, you can’t help that,' said the Cat: 'we’re all mad here. I’m mad. You’re mad.' " +
+                "'How do you know I’m mad?' said Alice. " +
+                "'You must be,' said the Cat, 'or you wouldn’t have come here.'";
 
-        printArray(testStanfordCoreNLP(text));
+
+        String[] sentences = testStanfordCoreNLP(text);
+        println();
+        println("StanfordCoreNLP:");
+        printArray(sentences);
+        println();
+        println("OpenNLP:");
         printArray(testOpenNLP(text));
+        println();
+        println("OpenNLP Positions:");
         printArray(testOpenNLPPosition(text));
     }
 
     private static <T> void printArray(T[] array) {
         Arrays.asList(array).stream().forEach(System.out::println);
+    }
+
+    private static <T> void println(T obj) {
+        System.out.println(obj);
+    }
+
+    private static void println() {
+        System.out.println();
     }
 
     private static StanfordCoreNLP getStanfordCoreNLP(){
